d0c081d0a93404c8f86a948e364627979049d071
akbarhps <akbarputrasrg@gmail.com>
2021-04-11 20:25:00 +0700

    init

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const formidable = require('formidable');
 require('dotenv').config()
 
 var app = express();
@@ -8,10 +9,21 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+app.post('/api/fileanalyse', (req, res) => {
+  const form = formidable({ multiples: true });
+  form.parse(req, (err, fields, files) => {
+    const attr = files.upfile;
+    res.json({
+      name: attr.name,
+      type: attr.type,
+      size: attr.size
+    });
+  });
+});
 
 
 const port = process.env.PORT || 3000;
